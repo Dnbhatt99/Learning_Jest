512ce630ef7e32894c918b6887b15483e8d4c289
Dnbhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-08-29 00:38:34 -0500

    Files Updated

--- sum.js
+++ sum.js
@@ -1,5 +1,14 @@
+const { default: axios } = require("axios");
+const { error } = require("console");
+
 function sum(num1, num2) {
     return num1 + num2
 }
 
-module.exports = sum;
+function fetchUser() {
+    axios.get('https://jsonplaceholder.typicode.com/Users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
+}
+
+module.exports = sum, fetchUser;
